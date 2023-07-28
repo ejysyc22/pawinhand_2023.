2bc7af5a37c2697ab75500a0910d89b67c10e6f9
ej <solbi7878@naver.com>
2023-07-28 17:38:22 +0900

    s0728

--- script/script.js
+++ script/script.js
@@ -25,3 +25,34 @@ m_nav.addEventListener('click',function(){
 close.addEventListener('click',function(){
     m_nav_open.style.display='none';
 }) 
+
+// main-swiper-slide
+// const 변수명 = new Swiper('적용대상');   
+// const 변수명 = new Swiper('적용대상',{속성:값, 속성:값});   
+const pawin_slide = new Swiper('#pawin_slide',{
+    // 자동재생 
+    autoplay:{//자동재생  /* 기본이 3초=3000 */ 
+        delay:1000,
+        disableOnInteraction:false,}, //버튼 클릭 후 자동재생유지
+    loop:true,          /* loop:true 자연스럽게 한방향으로 지나간다. */
+    effect:'fade', //제자리 변경 슬라이드 효과  /* effect나 direction 둘중 하나만 */
+    //direction:'vertical' 
+    navigation: {
+        //next,prev 객체 연결 시 부모를 안적으면 
+        //body 안에 있는 모든 swiper-next, prev를 인식하기 때문에
+        //개별인식가능한 부모이름을 반드시 안에 먼저 작성한다.
+        nextEl: '#pawin_slide .swiper-button-next',   /* #pawin_slide부모명을 적어줘서 전용으로 인식 */
+        prevEl: '#pawin_slide .swiper-button-prev',   
+    },
+    
+});
+
+/* const pawin_slide = new Swiper('#',{
+    autoplay:{delay:1000}, loop:true,        
+    effect:'fade',
+    navigation: {        
+        nextEl: '#pawin_slide .swiper-button-next',  
+        prevEl: '#pawin_slide .swiper-button-prev',   
+    },
+    
+}); */
